3b86377dbc3df740f28649f5d76d8cfb1c6ce369
Halyoun <XakacPavel@mail.ru>
2025-09-20 22:06:31 +0700

    add gitignore

--- main.go
+++ main.go
@@ -7,5 +7,7 @@ const usdToRub = 83.59
 const eurToRub = usdToRub / usdToEur
 
 func main() {
+	fmt.Println("Usd to Eur", usdToEur)
+	fmt.Println("Usd to Rub", usdToRub)
 	fmt.Println("Eur to Rub", eurToRub)
 }
