9d7000a50e57275f9bf24e28a400118fb15a51f5
Halyoun <XakacPavel@mail.ru>
2025-09-21 20:38:02 +0700

    Add new function

--- main.go
+++ main.go
@@ -7,7 +7,22 @@ const usdToRub = 83.59
 const eurToRub = usdToRub / usdToEur
 
 func main() {
-	fmt.Println("Usd to Eur", usdToEur)
-	fmt.Println("Usd to Rub", usdToRub)
-	fmt.Println("Eur to Rub", eurToRub)
+	userInput()
+}
+
+func userInput() (float64, string, string) {
+	var sum float64
+	var curr1 string
+	var curr2 string
+	fmt.Print("Введите валюту, из которой будет конвертация (usd/eur/rub): ")
+	fmt.Scanln(&curr1)
+	fmt.Print("Введите валюту, в которую хотите сконвертировать (usd/eur/rub): ")
+	fmt.Scanln(&curr2)
+	fmt.Print("Введите сумму: ")
+	fmt.Scanln(&sum)
+	return sum, curr1, curr2
+}
+
+func convertOper(sum float64, curr1 string, curr2 string) float64 {
+	return 0.0
 }
