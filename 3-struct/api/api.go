ab6f94cd10b2d00566cb4e8ebd59753ef2ab6237
Halyoun <XakacPavel@mail.ru>
2025-10-18 16:47:21 +0700

    Api

--- 3-struct/api/api.go
+++ 3-struct/api/api.go
@@ -1,9 +1,68 @@
 package api
 
 import (
+	"app-3/bins"
 	"app-3/config"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
 )
 
 func GetKey(cfg *config.Config) string {
 	return cfg.Key
 }
+
+func Operation(operation string) {
+	//var binlist = bins.BinList{}
+	switch operation {
+	case "create":
+		//CreateBin()
+	case "update":
+	}
+}
+
+func CreateBin(filename string, binlist *bins.BinList) {
+	binlist.ScanJson(filename)
+	postBody, err := json.Marshal(binlist)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	// Создаём кастомный POST-запрос
+	req, err := http.NewRequest("POST", "https://api.jsonbin.io/v3/b", bytes.NewBuffer(postBody))
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Устанавливаем заголовки
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("X-Master-Key", GetKey(config.NewConfig("Key.env")))
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusCreated {
+		body, _ := io.ReadAll(resp.Body)
+		log.Fatalf("Ошибка: статус %d\nОтвет: %s", resp.StatusCode, body)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal("Ошибка чтения ответа:", err)
+	}
+	fmt.Println("✅ Bin успешно создан!")
+	fmt.Println("Ответ сервера:", string(body))
+}
+
+func UpdateBin() {}
+
+func DeleteBin() {}
+
+func GetBin() {}
--- 3-struct/main.go
+++ 3-struct/main.go
@@ -9,8 +9,12 @@ import (
 )
 
 func main() {
-	BinList := bins.BinList{}
+	//operation := flag.String("operation", "list", "operations of bins")
+	//file := flag.String("file", "", "file name")
+	//nameOfBin := flag.String("name", "Untitled", "name of bin")
+	//flag.Parse()
 	var mg string
+	var BinList = bins.BinList{}
 	fmt.Println(api.GetKey(config.NewConfig("Key.env")))
 	fmt.Println("---Менеджер бинов---")
 Menu:
@@ -21,6 +25,7 @@ Menu:
 			BinList.ScanJson("data.json")
 			BinList.NewBin()
 			storage.SaveJson("data.json", BinList)
+			api.CreateBin("data.json", &BinList)
 		case "2":
 			BinList.ScanJson("data.json")
 			BinList.PrintJson()
