f943082fe7afb70f6e29a05dd90f58f7cc07bdc9
Halyoun <XakacPavel@mail.ru>
2025-10-18 17:32:38 +0700

    Готовый API

--- 3-struct/api/api.go
+++ 3-struct/api/api.go
@@ -11,20 +11,16 @@ import (
 	"net/http"
 )
 
+type jsonBinResponse struct {
+	Record   bins.BinList `json:"record"`
+	Metadata any          `json:"metadata"`
+}
+
 func GetKey(cfg *config.Config) string {
 	return cfg.Key
 }
 
-func Operation(operation string) {
-	//var binlist = bins.BinList{}
-	switch operation {
-	case "create":
-		//CreateBin()
-	case "update":
-	}
-}
-
-func CreateBin(filename string, binlist *bins.BinList) {
+func CreateBin(filename, nameBin string, binlist *bins.BinList) {
 	binlist.ScanJson(filename)
 	postBody, err := json.Marshal(binlist)
 	if err != nil {
@@ -33,13 +29,14 @@ func CreateBin(filename string, binlist *bins.BinList) {
 	}
 
 	// Создаём кастомный POST-запрос
-	req, err := http.NewRequest("POST", "https://api.jsonbin.io/v3/b", bytes.NewBuffer(postBody))
+	req, err := http.NewRequest("POST", "https://api.jsonbin.io/v3/b/"+"", bytes.NewBuffer(postBody))
 	if err != nil {
 		log.Fatal(err)
 	}
 	// Устанавливаем заголовки
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("X-Master-Key", GetKey(config.NewConfig("Key.env")))
+	req.Header.Set("X-Bin-Name", nameBin)
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
@@ -61,8 +58,106 @@ func CreateBin(filename string, binlist *bins.BinList) {
 	fmt.Println("Ответ сервера:", string(body))
 }
 
-func UpdateBin() {}
+func UpdateBin(filename, id string, binlist *bins.BinList) {
+	if id == "" {
+		fmt.Println("ID НЕ БЫЛ ВВЕДЕН")
+		return
+	}
+	binlist.ScanJson(filename)
+	postBody, err := json.Marshal(binlist)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
 
-func DeleteBin() {}
+	req, err := http.NewRequest("PUT", "https://api.jsonbin.io/v3/b/"+id, bytes.NewBuffer(postBody))
+	if err != nil {
+		log.Fatal(err)
+	}
 
-func GetBin() {}
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("X-Master-Key", GetKey(config.NewConfig("Key.env")))
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusCreated {
+		body, _ := io.ReadAll(resp.Body)
+		log.Fatalf("Ошибка: статус %d\nОтвет: %s", resp.StatusCode, body)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal("Ошибка чтения ответа:", err)
+	}
+	fmt.Println("✅ Bin успешно обновлен!")
+	fmt.Println("Ответ сервера:", string(body))
+}
+
+func DeleteBin(id string) {
+	if id == "" {
+		fmt.Println("ID НЕ БЫЛ ВВЕДЕН")
+		return
+	}
+	req, err := http.NewRequest("DELETE", "https://api.jsonbin.io/v3/b/"+id, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.Header.Set("X-Master-Key", GetKey(config.NewConfig("Key.env")))
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusCreated {
+		body, _ := io.ReadAll(resp.Body)
+		log.Fatalf("Ошибка: статус %d\nОтвет: %s", resp.StatusCode, body)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal("Ошибка чтения ответа:", err)
+	}
+	fmt.Println("✅ Bin успешно удален!")
+	fmt.Println("Ответ сервера:", string(body))
+}
+
+func GetBin(id string) (binlist bins.BinList) {
+	req, err := http.NewRequest("GET", "https://api.jsonbin.io/v3/b/"+id, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.Header.Set("X-Master-Key", GetKey(config.NewConfig("Key.env")))
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusCreated {
+		body, _ := io.ReadAll(resp.Body)
+		log.Fatalf("Ошибка: статус %d\nОтвет: %s", resp.StatusCode, body)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal("Ошибка чтения ответа:", err)
+	}
+	var response jsonBinResponse
+	if err := json.Unmarshal(body, &response); err != nil {
+		log.Fatal("Ошибка Unmarshal:", err)
+	}
+
+	return response.Record
+}
